a83db3f0c38709bc93a4a6876b148aef8cedcaaa
ZooShow <fedulzhuravlev@mail.ru>
2022-01-08 20:30:23 +1000

    create all necessary files

---
+++ app/src/main/java/com/example/recyclerview/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.recyclerview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = RecyclerViewAdapter(getContacts())
+    }
+    private fun getContacts(): List<String> {
+        return this.resources.getStringArray(R.array.contacts).toList()
+    }
+}
---
+++ app/src/main/java/com/example/recyclerview/RecyclerViewAdapter.kt
@@ -0,0 +1,31 @@
+package com.example.recyclerview
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class RecyclerViewAdapter(private  val items: List<String>) :RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>()
+{
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val largeTextView: TextView = itemView.findViewById(R.id.textViewLarge)
+        val smallTextView: TextView = itemView.findViewById(R.id.textViewSmall)
+        val imageView: ImageView = itemView.findViewById(R.id.imageView)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val itemView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.recyclerview_item, parent, false)
+        return ViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.largeTextView.text = items[position]
+        holder.smallTextView.text = "на телефоне"
+        holder.imageView.setImageResource(R.drawable.icon)
+    }
+
+    override fun getItemCount() = items.size
+}
